263316c728f03a3415c004c3a1513ba505fdddee
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-06-03 20:18:10 -0300

    Projeto Calc Final
    Script OK
    CSS OK
    HTML OK

--- Projetos/Calculadora/script.js
+++ Projetos/Calculadora/script.js
@@ -1,10 +1,30 @@
+var res = document.getElementById("resultado")
+
 function inserir(num){
-    res = document.getElementById("resultado")
-    res.innerHTML += num
+    if(Number(res.innerHTML.length) < 10){
+        res.innerHTML += num
+    } 
 }
-function reset (){
-    res.innerHTML = ''
+function reset(){
+    res.innerHTML = null
 }
 function inverter(){
-    
+    res.innerHTML *= (-1)
+}
+function porcem(){
+    res.innerHTML /= 100
+}
+var op1 = null
+var ope = null
+function operacao(op){
+    op1 = res.innerHTML
+    ope = op
+    res.innerHTML = ''
+}
+function calcular(){
+    var op2 = res.innerHTML
+    var resultado = `${op1}${ope}${op2}`
+    if(resultado){
+    document.getElementById('resultado').innerHTML = eval(resultado)
+    }
 }
