a07a3ec5ca56e9cbc8095db3bd4ec8d6a58873dd
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-06-09 11:08:14 -0300

    Update Script
    Maioria das Funções feitas
    Preciso terminar a função do botão de paginação "2nd"
    Começar a fazer o menu para deixar a página mais elegante

--- Projetos/Calculadora Científica/script.js
+++ Projetos/Calculadora Científica/script.js
@@ -1,10 +1,10 @@
-var res = document.getElementById("result")
-var ac = document.getElementById("ac")
-var w = document.documentElement.clientWidth;
-var deitado = 26
-var emPe = 8
+const res = document.getElementById("result")
+const ac = document.getElementById("ac")
+const mr = document.getElementById("mr")
+const w = document.documentElement.clientWidth;
 var baseElev = false
 var expo = false
+var memoria = 0
 
 function inserir(num){
     if(res.innerHTML === '0'){
@@ -15,12 +15,12 @@ function inserir(num){
     else{
         ac.innerHTML = 'C'
         if(w <= 850){
-            if(Number(res.innerHTML.length) < emPe){
+            if(Number(res.innerHTML.length) < 8){
                 res.innerHTML += num
             } 
         }
         else{
-            if(Number(res.innerHTML.length) < deitado){
+            if(Number(res.innerHTML.length) < 26){
                 res.innerHTML += num
             } 
         }
@@ -63,6 +63,19 @@ function ln(){
     var x = res.innerHTML
     res.innerHTML = Math.log(x)
 }
+function rad(){
+    res.innerHTML *= Math.PI/180
+
+
+
+
+}
+function rand(){
+    res.innerHTML = Math.random()
+}
+function pi(){
+    res.innerHTML = Math.PI
+}
 function log10(){
     var x = res.innerHTML
     res.innerHTML = Math.log10(x)
@@ -71,12 +84,69 @@ function elevado(){
     baseElev = res.innerHTML
     res.innerHTML = ''
 }
-
-function exponencial(){
+function vezesDezElevado(){
     expo = res.innerHTML
     res.innerHTML = ''
 }
-
+function memoriaMais(){
+    memoria += res.innerHTML
+    res.innerHTML = ''
+    mr.style.backgroundColor = '#5f5f5f';
+}
+function memoriaMenos(){
+    memoria -= res.innerHTML
+    res.innerHTML = ''
+    mr.style.backgroundColor = '#5f5f5f';
+}
+function mostraMemoria(){
+    res.innerHTML = memoria
+    mr.style.backgroundColor = '#212121'
+}
+function resetMemoria(){
+    memoria = 0
+    mr.style.backgroundColor = '#212121'
+}
+function exponencial(){
+    var num = res.innerHTML
+    for(var c = (num-1); c > 0;c--){
+        num *= c
+    }
+    res.innerHTML = num
+}
+function seno(){
+    var num = res.innerHTML
+    res.innerHTML = Math.sin(num)
+}
+function sinh(){
+    var num = res.innerHTML
+    res.innerHTML = Math.sinh(num)
+}
+function cosseno(){
+    var num = res.innerHTML
+    res.innerHTML = Math.cos(num)
+}
+function cosh(){
+    var num = res.innerHTML
+    res.innerHTML = Math.cosh(num)
+}
+function tangente(){
+    var num = res.innerHTML
+    res.innerHTML = Math.tan(num)
+}
+function tanh(){
+    var num = res.innerHTML
+    res.innerHTML = Math.tanh(num)
+}
+function functionE(){
+    if(res.innerHTML === '0'){
+        res.innerHTML = ''
+        res.innerHTML += Math.E
+        ac.innerHTML = 'C'
+    }
+    else{
+        res.innerHTML += Math.E
+    }
+}
 function calcular(){
     var resultado = res.innerHTML
     if(expo != false){
