a70bffc627db6bbe4ac52eefc9ee602c8e850fdb
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-07-09 22:17:10 -0300

    :sparkles: Contador Funcionando!

--- Estudos/Estudos_Alternativos/Let's Code/live/contador/script.js
+++ Estudos/Estudos_Alternativos/Let's Code/live/contador/script.js
@@ -1,60 +1,54 @@
 // Esta declaracao irá carregar as funcoes dentro dela apos o load completo da pagina
 window.onload = () => {
-    var botao = document.getElementById("botao")
+    let botao = document.getElementById("botao")
 
     botao.addEventListener("click", () => {
         const countDiv = document.getElementById("timer")
-        let minutosInput = document.getElementById("minutos")
-        let segundosInput = document.getElementById("segundos")
+        let minutosInput = Number(document.getElementById("minutos").value)
+        let segundosInput = Number(document.getElementById("segundos").value)
+        let esconde = document.getElementsByClassName("esconde")
+        let aparece = document.getElementById("input2")
+        
+        esconde[0].style.display = "none"
+        esconde[1].style.display = "none"
+        esconde[2].style.display = "none"
+        aparece.style.display = "none"
 
-        // Validacao caso dois campos nao estejam preenchidos
-        if(minutosInput.value == "" && segundosInput.value == ""){
-            countDiv.innerHTML = 'INSIRA ALGUM VALOR AOS CAMPOS ACIMA'
+        // Tempo em segundos
+        let segTotais = minutosInput*60 + segundosInput
+        console.log(segTotais)
+
+        // Funcao que ira decrescer os segundos e fazer as verificacoes necessarias
+        const tempoPassa = () => {
+            // Irá calcular quantos minutos faltam com base na variavel sec com floor que deixara numeros quebrados ao ultimo inteiro
+            let minutosRestantes = Math.floor(segTotais/60)
+
+            // Calculara o restante dos segundos
+            let segundosRestantes = segTotais % 60
+
+            // Validacao da formatacao
+            
+            if(segundosRestantes < 10){
+                segundosRestantes = "0" +segundosRestantes
+            }
+            if(minutosRestantes < 10){
+                minutosRestantes = "0" +minutosRestantes
+            } 
+
+            // Gerar o formato e imprimir na tela
+            countDiv.innerHTML = minutosRestantes + ":" + segundosRestantes
+
+            // Irá decrescer uma unidade
+            if(segTotais > 0){
+                segTotais = segTotais -1
+            }
+            else if(segTotais == 0){
+                countDiv.innerHTML = "Acabou o Tempo!"
+            }
+            
         }
-        else{
-            // Validacao caso um campo nao esteja preenchido
-            if(minutosInput.value == ""){
-                minutosInput = 0
-            }
-            if(segundosInput.value == ""){
-                segundosInput = 0
-            }
 
-            document.getElementsByClassName("esconde").style.display = "none"
-
-            document.getElementById("input2").style.display = "block"
-
-            // Tempo em segundos
-            let sec = (minutosInput*60)+segundosInput
-
-            // Funcao que ira decrescer os segundos e fazer as verificacoes necessarias
-            const tempoPassa = () => {
-                // Irá calcular quantos minutos faltam com base na variavel sec com floor que deixara numeros quebrados ao ultimo inteiro
-                let minutos = Math.floor(sec/60)
-
-                // Calculara o restante dos segundos
-                let segundosRestantes = sec % 60
-
-                // Validacao da formatacao
-                if(segundosRestantes < 10){
-                    segundosRestantes = "0" + segundosRestantes
-                }
-                if(minutos < 10){
-                    minutos = "0" + minutos
-                }
-
-                // Gerar o formato e imprimir na tela
-                countDiv.innerHTML = minutos + ":" + segundosRestantes
-
-                // Irá decrescer uma unidade
-                if(sec > 0){
-                    sec = sec -1
-                }
-                else{
-                    countDiv.innerHTML = "Acabou o Tempo!"
-                }
-            }
-            const countDown = setInterval(() => tempoPassa(), 1000)
-        }
+        const countDown = setInterval(() => tempoPassa(), 1000)    
+    
     })
 }
