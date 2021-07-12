8c81c345b131fe890b2f408c62a13fb01f75d032
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-07-12 15:27:56 -0300

    :tada: Um pouco de Animação em JS puro

---
+++ Estudos/Estudos_Alternativos/AnimacaoJS/script.js
@@ -0,0 +1,18 @@
+var iconMenu = document.getElementsByClassName("icon-menu")[0]
+
+iconMenu.addEventListener('click',() => {
+    // Efeito de opacidade
+    let menu = document.getElementById("menu")
+    
+    if(menu.classList.contains("hide")){
+        menu.classList.add("show")
+        menu.classList.remove("hide")
+    }
+    else{
+        menu.classList.add("hide")
+        menu.classList.remove("show") 
+    }
+
+    // Efeito Toggle
+    // Show e Hide com Height ou Width
+})
